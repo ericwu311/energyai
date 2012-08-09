1e23355eab3a462bd9dd33765f3a2dc584c88a5c
Mark Chung <mark.y.chung@gmail.com>
2012-08-08 18:36:05 -0700

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,24 +39,22 @@ describe User do
 	# it { should respond_to(:reverse_relationships) }	
 	# it { should respond_to(:followers) }	
 	it { should be_valid }
+ 	it { should_not be_admin }
 
-
-#  	it { should_not be_admin }
-# 		describe "accessible attributes" do
-# 		it "should not allow access to admin" do
-# 			expect do
-# 				User.new(admin: @user.admin)
-# 			end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
-# 		end
-# 	end
-# end
+	describe "accessible attributes" do
+		it "should not allow access to admin" do
+			expect do
+				User.new(admin: @user.admin)
+			end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+		end
+	end
 
  	describe "with admin attribute set to 'true'" do
  		before do
  			@user.save!
  			@user.toggle!(:admin)
  		end
- 		
+
  		it { should be_admin }
  	end
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,71 +51,94 @@ describe "AuthenticationPages" do
 	 			it { should_not have_link('Settings', href: edit_user_path(user)) }
  			end
  		end
+	end
 
- 		describe "authorization" do
+	describe "authorization" do
 
- 			describe "for non-signed-in users" do
- 				let(:user) { FactoryGirl.create(:user) }
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
 
-				describe "when attempting to visit a protected page" do
-					before do
-						visit edit_user_path(user)
-						sign_in(user)
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					sign_in(user)
+				end
+
+				describe "after signing in" do
+
+					it "should render the desired protected page" do
+						page.should have_selector('title', text: "Edit #{user.name}")
 					end
 
-					describe "after signing in" do
-
-						it "should render the desired protected page" do
-							page.should have_selector('title', text: "Edit #{user.name}")
+					describe "when signing in again" do
+						before do
+							visit signin_path
+							sign_in(user)
 						end
 
-						describe "when signing in again" do
-							before do
-								visit signin_path
-								sign_in(user)
-							end
-
-							it "should render the default profile page" do
-								page.should have_selector('title', text: user.name)
-							end
+						it "should render the default profile page" do
+							page.should have_selector('title', text: user.name)
 						end
 					end
 				end
- 				
- 				describe "in the Users controller" do
+			end
+				
+			describe "in the Users controller" do
 
- 					describe "visiting the edit page" do
- 						before { visit edit_user_path(user) }
- 						it { should have_selector('title', text: 'Sign in') }
- 					end
-
- 					describe "submitting to the update action" do
- 						before { put user_path(user) }
- 						specify { response.should redirect_to(signin_path) }
- 					end
-
- 					describe "visiting the user index" do
-						before { visit users_path }
-						it { should have_selector('title', text: 'Sign in') }
-					end
- 				end
- 			end
-
- 			describe "as wrong user" do
-				let(:user) { FactoryGirl.create(:user) }
-				let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-				before { sign_in user }
-
-				describe "visiting Users#edit page" do
-					before { visit edit_user_path(wrong_user) }
-					it { should_not have_selector('title', text: full_title("Edit #{wrong_user.name}")) }
+				describe "visiting the edit page" do
+					before { visit edit_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
 				end
 
-				describe "submitting a PUT request to the Users#update action" do
-					before { put user_path(wrong_user) }
-					specify { response.should redirect_to(root_path) }
+				describe "submitting to the update action" do
+					before { put user_path(user) }
+					specify { response.should redirect_to(signin_path) }
 				end
-    		end
- 		end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+			end
+		end
+
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+			before { sign_in user }
+
+			describe "visiting Users#edit page" do
+				before { visit edit_user_path(wrong_user) }
+				it { should_not have_selector('title', text: full_title("Edit #{wrong_user.name}")) }
+			end
+
+			describe "submitting a PUT request to the Users#update action" do
+				before { put user_path(wrong_user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		end
+
+		describe "as non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) {FactoryGirl.create(:user) }
+
+			before { sign_in non_admin }
+			
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		end
+
+		describe "as an admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:admin) { FactoryGirl.create(:admin) }
+
+			before {sign_in admin }
+
+			it "shouldn't be able to self destruct on DELETE request" do
+				expect { delete user_path(admin) }.not_to change(User, :count).by(-1)
+			end
+		end
  	end
 end
